93864456979e8aa697cbc6f485eadf7c3fa0ffd1
Abdul Rojak <122954244+bigzero55@users.noreply.github.com>
2024-08-21 17:13:03 +0700

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const participantsRoute = require("./participants");
-const StocksRouter = require("./stocks");
-const salesRouter = require("./sales");
-const Consuments = require("./consuments");
-const Suplayers = require("./suplayers");
-const Return = require("./retur");
-const Analystic = require("./analystic");
-
-// router.use("/stocks", StocksRouter);
-// router.use("/consuments", Consuments);
-// router.use("/suplayers", Suplayers);
-// router.use("/sales", salesRouter);
-// router.use("/retur", Return);
-// router.use("/analystic", Analystic);
+const scannerRoute = require("./scan");
+const sessionRoute = require("./sessions");
 
 router.use("/participants", participantsRoute);
+router.use("/scanner", scannerRoute);
+router.use("/sesi", sessionRoute);
 
 router.get("/status", (req, res) => {
   res.json({
